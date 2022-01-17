e45f4bc1f83d57bfe5176bff17b9c3597eb67ca7
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-01-17 09:33:03 +0200

    Groups & orgs are sorted per lang

--- src/main/java/fi/vm/yti/terminology/api/frontend/FrontendController.java
+++ src/main/java/fi/vm/yti/terminology/api/frontend/FrontendController.java
@@ -219,17 +219,17 @@ public class FrontendController {
     @Operation(summary = "Get organization list", description = "Get organizations available at YTI Group Management Service")
     @ApiResponse(responseCode = "200", description = "The basic info for organizations in unprocessed Termed JSON format")
     @GetMapping(path = "/organizations", produces = APPLICATION_JSON_VALUE)
-    JsonNode getOrganizationList() {
+    JsonNode getOrganizationList(@RequestParam(defaultValue = "fi") String language) {
         logger.info("GET /organizations requested");
-        return termedService.getNodeListWithoutReferencesOrReferrers(Organization);
+        return termedService.getNodeListWithoutReferencesOrReferrers(Organization, language);
     }
 
     @Operation(summary = "Get information domain list")
     @ApiResponse(responseCode = "200", description = "Information domain list in unprocessed Termed JSON format")
     @GetMapping(path = "/groups", produces = APPLICATION_JSON_VALUE)
-    JsonNode getGroupList() {
+    JsonNode getGroupList(@RequestParam(defaultValue = "fi") String language) {
         logger.info("GET /groups requested");
-        return termedService.getNodeListWithoutReferencesOrReferrers(Group);
+        return termedService.getNodeListWithoutReferencesOrReferrers(Group, language);
     }
 
     @Operation(summary = "Make a bulk modification request", description = "Update and/or delete several nodes")
