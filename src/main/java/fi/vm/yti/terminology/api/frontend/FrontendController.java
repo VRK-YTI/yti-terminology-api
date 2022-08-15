942e7980499f1c0f4c07c4c60d15c186da0e21ca
Tomi Matsumoto-Viljanen <tomi.matsumoto-viljanen@gofore.com>
2022-08-15 09:16:57 +0300

    Added length validation to most properties in nodetypes. Lengths are based on the UI. See YTI-2012

--- src/main/java/fi/vm/yti/terminology/api/frontend/FrontendController.java
+++ src/main/java/fi/vm/yti/terminology/api/frontend/FrontendController.java
@@ -1,46 +1,35 @@
 package fi.vm.yti.terminology.api.frontend;
 
-import java.util.*;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fi.vm.yti.security.AuthenticatedUserProvider;
+import fi.vm.yti.security.YtiUser;
 import fi.vm.yti.terminology.api.exception.NamespaceInUseException;
 import fi.vm.yti.terminology.api.exception.VocabularyNotFoundException;
 import fi.vm.yti.terminology.api.frontend.searchdto.*;
+import fi.vm.yti.terminology.api.model.termed.*;
 import fi.vm.yti.terminology.api.validation.ValidGenericDeleteAndSave;
 import fi.vm.yti.terminology.api.validation.ValidVocabularyNode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import fi.vm.yti.security.AuthenticatedUserProvider;
-import fi.vm.yti.security.YtiUser;
-import fi.vm.yti.terminology.api.model.termed.GenericDeleteAndSave;
-import fi.vm.yti.terminology.api.model.termed.GenericNode;
-import fi.vm.yti.terminology.api.model.termed.GenericNodeInlined;
-import fi.vm.yti.terminology.api.model.termed.Graph;
-import fi.vm.yti.terminology.api.model.termed.Identifier;
-import fi.vm.yti.terminology.api.model.termed.MetaNode;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Size;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import static fi.vm.yti.terminology.api.model.termed.NodeType.Group;
 import static fi.vm.yti.terminology.api.model.termed.NodeType.Organization;
+import static fi.vm.yti.terminology.api.validation.ValidationConstants.TEXT_FIELD_MAX_LENGTH;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -211,7 +200,7 @@ public class FrontendController {
             @Parameter(description = "The meta model graph for the new terminology")
             @RequestParam UUID templateGraphId,
 
-            @Size(min = 3, message = "Prefix must be minimum of 3 characters")
+            @Size(min = 3, max = TEXT_FIELD_MAX_LENGTH, message = "Prefix must be minimum of 3 characters and a maximum of " + TEXT_FIELD_MAX_LENGTH + " characters")
             @Parameter(description = "The prefix, i.e., freely selectable part of terminology namespace")
             @RequestParam String prefix,
 
