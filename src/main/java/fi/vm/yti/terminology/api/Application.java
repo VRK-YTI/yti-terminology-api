46e48a68412597ba51a8dcc24119f17f1d83fd20
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2022-11-02 09:09:10 +0200

    login debug

--- src/main/java/fi/vm/yti/terminology/api/Application.java
+++ src/main/java/fi/vm/yti/terminology/api/Application.java
@@ -1,10 +1,18 @@
 package fi.vm.yti.terminology.api;
 
+import java.util.Arrays;
 import java.util.concurrent.Executor;
+import java.util.stream.StreamSupport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -63,5 +71,23 @@ public class Application {
         executor.setThreadNamePrefix("ImportAPI-");
         executor.initialize();
         return executor;
+    }
+
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        final Environment env = event.getApplicationContext()
+                .getEnvironment();
+
+        System.out.println(env.getActiveProfiles());
+
+        final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+
+        StreamSupport.stream(sources.spliterator(), false)
+                .filter(ps -> ps instanceof EnumerablePropertySource)
+                .map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
+                .flatMap(Arrays::stream)
+                .distinct()
+                .filter(prop -> !(prop.contains("credentials") || prop.contains("password")))
+                .forEach(prop -> System.out.println(prop + ": " + env.getProperty(prop)));
     }
 }
