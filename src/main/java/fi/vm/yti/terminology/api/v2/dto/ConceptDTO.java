07936d60b74710d3f1e86275f0ed324492f4a5b3
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2024-03-27 08:57:14 +0200

    refactoring

--- src/main/java/fi/vm/yti/terminology/api/v2/dto/ConceptDTO.java
+++ src/main/java/fi/vm/yti/terminology/api/v2/dto/ConceptDTO.java
@@ -3,9 +3,9 @@ package fi.vm.yti.terminology.api.v2.dto;
 import java.util.List;
 import java.util.Map;
 
-import fi.vm.yti.common.dto.ResourceBaseDTO;
+import fi.vm.yti.common.dto.BaseDTO;
 
-public class ConceptDTO extends ResourceBaseDTO {
+public class ConceptDTO extends BaseDTO {
     private Map<String, String> definition;
     private List<TermDTO> terms;
 
--- src/main/java/fi/vm/yti/terminology/api/v2/dto/TerminologyDTO.java
+++ src/main/java/fi/vm/yti/terminology/api/v2/dto/TerminologyDTO.java
@@ -2,16 +2,5 @@ package fi.vm.yti.terminology.api.v2.dto;
 
 import fi.vm.yti.common.dto.MetaDataDTO;
 
-import java.util.Set;
-
 public class TerminologyDTO extends MetaDataDTO {
-    private Set<String> languages;
-
-    public Set<String> getLanguages() {
-        return languages;
-    }
-
-    public void setLanguages(Set<String> languages) {
-        this.languages = languages;
-    }
 }
